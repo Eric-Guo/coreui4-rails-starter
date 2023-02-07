860ce98fe714723395fd9244dc654708bea274e2
Eric Guo <eric.guocz@gmail.com>
2023-02-07 16:52:28 +0800

    Extract the wrapper class.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :set_page_layout_data, if: -> { request.format.html? }
+
+  protected
+
+  def set_page_layout_data
+    @_wrapper_class = %w[wrapper d-flex flex-column min-vh-100 bg-light]
+  end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,10 @@
 class HomeController < ApplicationController
   def root
   end
+
+  protected
+
+  def set_page_layout_data
+    @_wrapper_class = %w[bg-light min-vh-100 d-flex flex-row align-items-center dark:bg-transparent]
+  end
 end
