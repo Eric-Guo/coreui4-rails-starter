64818a91c289faf049f5f45f16855b08084a3cbb
Eric Guo <eric.guocz@gmail.com>
2023-02-08 14:22:44 +0800

    rails g devise User

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show"
 
+  devise_for :users
+
   # Defines the root path route ("/")
   root "home#root"
 end
