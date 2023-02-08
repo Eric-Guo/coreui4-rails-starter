85da366a37e9179baa90c0d7e07984e775effabe
Eric Guo <eric.guocz@gmail.com>
2023-02-08 14:30:36 +0800

    All user must sign in.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   include SidebarHelper
+  before_action :authenticate_user!
   before_action :set_page_layout_data, if: -> { request.format.html? }
 
   protected
--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,8 +1,16 @@
 require "test_helper"
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
-  test "should get root" do
+  include Devise::Test::IntegrationHelpers
+
+  test "should get root after sign in" do
+    sign_in users(:guochunzhong)
     get root_url
     assert_response :success
+  end
+
+  test "should redirect to sign in page if not sign in" do
+    get root_url
+    assert_response :redirect
   end
 end
