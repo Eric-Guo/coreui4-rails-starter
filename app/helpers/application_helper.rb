bcba4124865701b91bdf7041d9cd951ce7b18692
Eric Guo <eric.guocz@gmail.com>
2023-02-09 16:29:31 +0800

    Add footer in UI.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,20 @@ module ApplicationHelper
       content_tag :use, nil, "xlink:href": asset_path(cil_name)
     end
   end
+
+  def footer
+    content_tag :footer, nil, class: "footer" do
+      left_part = content_tag :div, nil do
+        concat link_to "CoreUI", "https://coreui.io"
+        concat " "
+        concat link_to "Rails Starter Template", "https://git.thape.com.cn/Eric-Guo/coreui-pro-rails-starter"
+        concat "  © 2023 Eric-Guo."
+      end
+      right_part = content_tag :div, nil, class: "ms-auto" do
+        concat "Powered by "
+        concat link_to "CoreUI PRO UI Components", "https://coreui-doc.redwoodjs.cn/"
+      end
+      left_part.concat(right_part)
+    end
+  end
 end
