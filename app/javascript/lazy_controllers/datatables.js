53c2e839f283f2d2751d0cfb572c55da6cfd330b
Eric-Guo <eric.guocz@gmail.com>
2023-02-24 20:41:14 +0800

    Put data-controller="datatables" to container node, to avoid double connect in Stimulus

--- app/javascript/lazy_controllers/datatables.js
+++ app/javascript/lazy_controllers/datatables.js
@@ -3,30 +3,27 @@ import { Controller } from "@hotwired/stimulus"
 Stimulus.register("datatables", class extends Controller {
   static targets = [ "datatable" ];
   static values = {
+    source: String,
     dtUrl: String,
     columns: Array
   }
 
   connect() {
-    if (typeof this.datatable == "undefined") {
-      const columns = this.columnsValue.map(function(m) { return {"data": m} });
-      this.datatable = $(this.datatableTarget).DataTable({
-        "serverSide": true,
-        "ajax": {
-          "url": $(this.datatableTarget).data('source')
-        },
-        language: {
-          "url": this.dtUrlValue
-        },
-        "pagingType": "full_numbers",
-        "columns": columns
-      });
-    }
+    const columns = this.columnsValue.map(function(m) { return {"data": m} });
+    this.datatable = $(this.datatableTarget).DataTable({
+      "serverSide": true,
+      "ajax": {
+        "url": this.sourceValue
+      },
+      language: {
+        "url": this.dtUrlValue
+      },
+      "pagingType": "full_numbers",
+      "columns": columns
+    });
   }
 
   disconnect() {
-    if (this.datatable.keys != undefined) {
-      this.datatable.destroy();
-    }
+    this.datatable.destroy();
   }
-})
+});
