31f2e0008be9dfd2f5093771ec6d002bc6b01b68
Eric-Guo <eric.guocz@gmail.com>
2023-03-06 21:27:07 +0800

    New columns preferred_page_length

--- db/migrate/20230220031241_add_preferred_language_to_user.rb
+++ db/migrate/20230220031241_add_preferred_language_to_user.rb
@@ -1,5 +1,6 @@
 class AddPreferredLanguageToUser < ActiveRecord::Migration[7.1]
   def change
     add_column :users, :preferred_language, :string
+    add_column :users, :preferred_page_length, :integer, default: 10, null: false
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -40,6 +40,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_03_01_155126) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "preferred_language"
+    t.integer "preferred_page_length", default: 10, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
