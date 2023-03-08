11e100427ff325c83df5e35ad901a3cdc74343e8
Eric-Guo <eric.guocz@gmail.com>
2023-03-08 22:32:46 +0800

    Model dialog will refresh whole page if required.

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -2,6 +2,10 @@ import { Controller } from "@hotwired/stimulus"
 import { get } from '@rails/request.js'
 
 export default class extends Controller {
+  static values = {
+    pageReload: Boolean
+  }
+
   click(e) {
     const target_url = e.target.getAttribute('href') || e.target.parentNode.getAttribute('href') || e.target.parentNode.parentNode.getAttribute('href');
     get(target_url).then((response) => {
@@ -13,7 +17,11 @@ export default class extends Controller {
           const modal = new coreui.Modal('#coreuiModal');
           modal.show();
           htmlContent.addEventListener('hidden.coreui.modal', event => {
-            this.dispatch("reloadDT", { detail: {} });
+            if(this.pageReloadValue) {
+              location.reload();
+            } else {
+              this.dispatch("reloadDT", { detail: {} });
+            }
           });
         });
       }
