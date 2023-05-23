2bb05b852c5aa2e95a0ff4d87ec8a1c6d97de160
Eric Guo <eric.guocz@gmail.com>
2023-05-23 16:44:44 +0800

    Enable tooltips everywhere
    https://coreui-doc.redwoodjs.cn/components/tooltips/#example-enable-tooltips-everywhere

--- app/javascript/packs/stimulus.js
+++ app/javascript/packs/stimulus.js
@@ -5,3 +5,6 @@ import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers"
 window.Stimulus = Application.start()
 const context = require.context("../controllers", true, /\.js$/)
 Stimulus.load(definitionsFromContext(context))
+
+const tooltipTriggerList = document.querySelectorAll('[data-coreui-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new coreui.Tooltip(tooltipTriggerEl))
