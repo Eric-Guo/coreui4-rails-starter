46c7ba6fce3e266d4c4ccb2cc1ce4ba0c62162c6
Eric Guo <eric.guocz@gmail.com>
2023-08-03 13:44:54 +0800

    Fix testing.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_03_01_155126) do
+ActiveRecord::Schema[7.1].define(version: 2023_08_03_034248) do
   create_table "roles", force: :cascade do |t|
     t.string "role_name"
     t.datetime "created_at", null: false
