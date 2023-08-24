9de3f3505469b460c88640d40cd370cdd0f9b44b
Eric Guo <eric.guocz@gmail.com>
2023-08-24 13:58:22 +0800

    Add support of rotate 180 for icon.

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -1,16 +1,18 @@
 module NavigationHelper
-  def nav_item(link_text, link_path, cil_name = nil, html_options = nil)
+  def nav_item(link_text, link_path, cil_name = nil, html_options = {})
     nav_link_class = if current_page?(link_path)
       "nav-link active"
     else
       "nav-link"
     end
     content_tag :li, nil, class: "nav-item" do
-      link_to link_path, {class: nav_link_class}, html_options do
+      nav_class = html_options[:nav_class]
+      html_option = html_options.except("nav_class")
+      link_to link_path, {class: nav_link_class}, html_option do
         if cil_name.nil?
           concat content_tag :span, nil, class: "nav-icon"
         else
-          concat content_tag :svg, content_tag(:use, nil, "xlink:href": asset_path(cil_name)), class: "nav-icon"
+          concat content_tag :svg, content_tag(:use, nil, "xlink:href": asset_path(cil_name)), class: ["nav-icon", nav_class]
         end
         concat link_text
         concat capture { yield } if block_given?
