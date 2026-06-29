eb4a85a960c3195ab0640bf9cf76bf78c8b4310b
Eric-Guo <eric.guocz@gmail.com>
2026-06-29 11:50:19 +0800

    Add user policy scope and sign-in tests

--- test/policies/user_policy_test.rb
+++ test/policies/user_policy_test.rb
@@ -2,8 +2,15 @@ require "test_helper"
 
 class UserPolicyTest < ActiveSupport::TestCase
   def test_scope
+    user = users(:user_fangzixue)
+
+    assert_equal [user], UserPolicy::Scope.new(user, User).resolve.to_a
   end
 
   def test_sign_in_as
+    target_user = users(:user_fangzixue)
+
+    assert UserPolicy.new(users(:user_guochunzhong), target_user).sign_in_as?
+    assert_not UserPolicy.new(target_user, users(:user_guochunzhong)).sign_in_as?
   end
 end
